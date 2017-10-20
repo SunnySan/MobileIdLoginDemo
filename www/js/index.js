71ca1e4fe35966363b2d7cab6c7beeca59ad9acb
SunnySan <sunny1227@pchome.com.tw>
2017-10-20 14:43:54 +0800

    1

---
+++ www/js/index.js
@@ -0,0 +1,5 @@
+﻿onDeviceReady: function() {
+	app.receivedEvent('deviceready');
+	window.open = cordova.InAppBrowser.open;
+	cordova.InAppBrowser.open('http://cms.gslssd.com/mobilebankcn/', '_self', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
+},
